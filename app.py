4b301ef3ce2ed5da5688f0a139cf8ffa1d0fb9a9
Ayush Kumar <coder103466@gmail.com>
2026-03-20 17:45:16 +0530

    Fix Render port issue

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def admin():
     return render_template("admin.html", data=data)
     
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
